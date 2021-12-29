4d416df97393d53034ed5ba98df17bf46f06c2be
contra.chen <1095695904@qq.com>
2021-12-29 10:08:30 +0800

    点亮星星

--- app.py
+++ app.py
@@ -46,10 +46,6 @@ class BinWebsite(db.Model):
     address = db.Column(db.String(192))
     binfolder_id = db.Column(db.Integer, db.ForeignKey("binfolders.id"))  # 设置外键
 
-@app.route('/',methods=["GET","POST"])
-def home():
-    return render_template('home.html')
-
 #注册函数
 @app.route('/register',methods=["GET","POST"])
 def register():
@@ -68,10 +64,9 @@ def register():
             new_user = User(username=username,password=password)
             db.session.add_all([new_user])
             db.session.commit()
-            return render_template('register.html', msg='添加成功')
+            return render_template('login.html', error='注册成功，欢迎登录')
 
-#登录界面
-@app.route('/login',methods=["GET","POST"])
+@app.route('/',methods=["GET","POST"])
 def login():
     #request：请求对象
     if request.method == 'GET':
@@ -86,8 +81,67 @@ def login():
         else:
             return render_template('login.html',error='用户名或密码错误')
 
-@app.route('/save')
-def index():
-    return "恭喜登录成功"
+@app.route('/save',methods=["GET","POST"])
+def save():
+    folders = Folder.query.all()
+    if request.method == 'GET':
+        return render_template('save.html', folders=folders)
+    folder_name = request.form.get('folder_name')
+    website_name = request.form.get('website_name')
+    website_address = request.form.get('website_address')
+    if not all([folder_name, website_name, website_address]):
+        return render_template('save.html', msg='请输入表单', folders=folders)
+    folder = Folder.query.filter_by(name=folder_name).first()
+    if folder:
+        website = Website.query.filter_by(address=website_address).first()
+        if website:
+            return render_template('save.html', msg='网址已存在', folders=folders)
+        new_website = Website(name=website_name, address=website_address)
+        folder.websites.append(new_website)
+        db.session.add(new_website)
+        db.session.commit()
+        folders = Folder.query.all()
+        return render_template('save.html', msg='添加成功', folders=folders)
+    else:
+        website = Website.query.filter_by(address=website_address).first()
+        if website:
+            return render_template('save.html', msg='网址已存在', folders=folders)
+        new_folder = Folder(name=folder_name)
+        new_website = Website(name=website_name, address=website_address)
+        new_folder.websites.append(new_website)
+        db.session.add(new_website)
+        db.session.add_all([new_folder])
+        db.session.commit()
+        folders = Folder.query.all()
+        return render_template('save.html', msg='添加成功', folders=folders)
+
+@app.route("/star")
+def star():
+    wid = request.args.get('wid')
+    website = Website.query.get(wid)
+    # result = Book.query.filter(Book.id == '1').first()
+    website.star = 1-website.star
+    db.session.commit()
+    return redirect("/save")
+
+@app.route("/delete_website")
+def delete_website():
+    wid = request.args.get('wid')
+    website = Website.query.get(wid)
+    db.session.delete(website)
+    db.session.commit()
+    return redirect("/save")
+
+@app.route("/delete_folder")
+def delete_author():
+    fid = request.args.get('fid')
+    folder = Folder.query.get(fid)
+    websites=folder.websites
+    for website in websites:
+        db.session.delete(website)
+    db.session.delete(folder)
+    db.session.commit()
+    return redirect("/save")
+
 if __name__ == '__main__':
     app.run()
